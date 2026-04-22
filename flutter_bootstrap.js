631f7250c9f0afb3afee49af625d1ca9303af4be
Hamed Rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2026-04-23 00:46:29 +0200

    release 1.4.6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1818549434" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2641611074" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
